3d0b9b164e9b02bde419ba0a9c7292c349275c7d
1lostevil1 <ki-04@yandex.ru>
2023-10-08 18:17:33 +0300

    im smort

--- src/main/java/edu/project1/Main.java
+++ src/main/java/edu/project1/Main.java
@@ -14,7 +14,6 @@ public final class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         LOGGER.info("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 0; i <= 2; i++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
