026c85a2277d6b5eb887950805f61b2729356107
1lostevil1 <ki-04@yandex.ru>
2023-10-08 19:00:33 +0300

    im smort

--- src/main/java/edu/hw1/Main.java
+++ src/main/java/edu/hw1/Main.java
@@ -12,7 +12,6 @@ public final class Main {
     }
 
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         LOGGER.info("Hello and welcome!");
 
