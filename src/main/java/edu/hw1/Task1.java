c9fdeec3f10166dc26ba47241641fe21a62bb266
1lostevil1 <ki-04@yandex.ru>
2023-10-08 23:05:38 +0300

    task01-try2

--- src/main/java/edu/hw1/Task1.java
+++ src/main/java/edu/hw1/Task1.java
@@ -1,44 +1,47 @@
 package edu.hw1;
+
 import java.util.Scanner;
 
 public class Task1 {
-    public static int minutesToSeconds(String str){
-        int result ;
-        if(check(str))
-        {
-            int pos=str.indexOf(':');
-            result = Integer.parseInt(str.substring(0, pos)) * 60 + Integer.parseInt(str.substring(pos+1));
+    public static int minutesToSeconds(String str) {
+        int result;
+        if (check(str)) {
+            int pos = str.indexOf(':');
+            result = Integer.parseInt(str.substring(0, pos)) * 60 + Integer.parseInt(str.substring(pos + 1));
+        } else {
+            result = -1;
         }
-        else result=-1;
         return result;
     }
-    Task1(){
+
+    Task1() {
     }
-    public static boolean check(String str){
+
+    public static boolean check(String str) {
         boolean flag = true;
         int len = str.length();
-        Integer pos=str.indexOf(':');
-        if(pos.equals(-1) || pos.equals(len-1)) flag=false;
-        if(flag)
-        {
-            String checkmin = str.substring(0,str.indexOf(':'));
-        String checksec = str.substring(str.indexOf(':')+1,len);
-        if(checkmin.length() <2 || checksec.length()!=2 || Integer.parseInt(checksec)>60)
-        {
-            flag=false;
+        Integer pos = str.indexOf(':');
+        if (pos.equals(-1) || pos.equals(len - 1)) {
+            flag = false;
         }
+        if (flag) {
+            String checkmin = str.substring(0, str.indexOf(':'));
+            String checksec = str.substring(str.indexOf(':') + 1, len);
+            if (checkmin.length() < 2 || checksec.length() != 2 || Integer.parseInt(checksec) > 60) {
+                flag = false;
+            }
         }
-        boolean flag2=false;
-        for(int i=0; i<len && flag;++i){
+        boolean flag2 = false;
+        for (int i = 0; i < len && flag; ++i) {
 
-            if (str.charAt(i)==':' && !flag2) {
-                flag2=true;
-            } else if(str.charAt(i)<'0'|| str.charAt(i)>'9') {
-                flag=false;
+            if (str.charAt(i) == ':' && !flag2) {
+                flag2 = true;
+            } else if (str.charAt(i) < '0' || str.charAt(i) > '9') {
+                flag = false;
             }
 
         }
-        return (flag && flag2 && str.charAt(0)!=':');
+        return (flag && flag2 && str.charAt(0) != ':');
     }
 
     public static void main(String[] args) {
