4d1c082c27ceaec42b36e523b521eb45a8cfaedf
1lostevil1 <ki-04@yandex.ru>
2023-10-15 16:04:57 +0300

    HOMEWORK second

---
+++ src/main/java/edu/hw2/Expr.java
@@ -0,0 +1,40 @@
+package edu.hw2;
+
+public sealed interface Expr permits Expr.Addition, Expr.Constant, Expr.Exponent, Expr.Multiplication, Expr.Negate {
+    double evaluate();
+
+    public record Constant(double value) implements Expr {
+        public double evaluate() {
+            return value;
+        }
+    }
+
+    public record Addition(Expr first, Expr second) implements Expr {
+        public double evaluate() {
+            return first.evaluate() + second.evaluate();
+        }
+    }
+
+    public record Negate(Expr value) implements Expr {
+        public double evaluate() {
+            return (-1) * value.evaluate();
+        }
+    }
+
+    public record Multiplication(Expr first, Expr second) implements Expr {
+        public double evaluate() {
+            return first.evaluate() * second.evaluate();
+        }
+    }
+
+    public record Exponent(Expr val, int exp) implements Expr {
+        public double evaluate() {
+            return Math.pow(val.evaluate(), exp);
+        }
+
+    }
+}
+
+
+
+
