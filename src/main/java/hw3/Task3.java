2c286e85ccf7a9f63d744a86ff93d948bca83100
1lostevil1 <ki-04@yandex.ru>
2023-10-26 18:39:11 +0300

    hw3-task3

--- src/main/java/hw3/Task3.java
+++ src/main/java/hw3/Task3.java
@@ -1,17 +1,19 @@
 package hw3;
 
+import org.apache.logging.log4j.LogManager;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Arrays;
 
 public class Task3 {
     private Task3() {
     }
 
-    public static HashMap<String, Integer> freqDict(List<String> list) {
-        HashMap<String, Integer> map = new HashMap<>();
+    public static HashMap<String, Integer> freqDict(List<?> list) {
+        HashMap< String, Integer> map = new HashMap<>();
         String s;
         for (int i = 0; i < list.size(); i++) {
-            s = "\"" + list.get(i) + "\"";
+            s = "\"" + list.get(i).toString() + "\"";
             if (map.containsKey(s)) {
                 map.replace(s, map.get(s), map.get(s) + 1);
             } else {
@@ -19,5 +21,13 @@ public class Task3 {
             }
         }
         return map;
+    }
+
+    private final static org.apache.logging.log4j.Logger LOGGER = LogManager.getLogger();
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 1, 1, 1, 2);
+        HashMap< String, Integer> map = freqDict(list);
+
+        LOGGER.info(map);
     }
 }
