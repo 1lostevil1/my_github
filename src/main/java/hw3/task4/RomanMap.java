214e03352e16e44f4179f51a162cc602ede8e931
1lostevil1 <ki-04@yandex.ru>
2023-10-26 17:03:46 +0300

    hw3-task4

--- src/main/java/hw3/task4/RomanMap.java
+++ src/main/java/hw3/task4/RomanMap.java
@@ -7,18 +7,18 @@ import java.util.List;
 
 public class RomanMap {
 
-    private final static Integer[] array = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    private final static String[] array2 = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    private final static Integer[] KeyArray = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private final static String[] ValArray = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
     public static HashMap<Integer, String> map;
     public static List<Integer> list;
 
     public RomanMap() {
         map = new HashMap<>();
         list = new ArrayList<>();
-        Collections.addAll(list, array);
-        int val = array.length;
+        Collections.addAll(list, KeyArray);
+        int val = KeyArray.length;
         for (int i = 0; i < val; i++) {
-            map.put(list.get(i), array2[i]);
+            map.put(list.get(i), ValArray[i]);
         }
     }
 }
