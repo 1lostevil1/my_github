a4afc835cd40b49152028db3d777d971357c2c12
1lostevil1 <ki-04@yandex.ru>
2023-10-28 21:37:21 +0300

    hw3-task7

--- src/main/java/hw3/Task3.java
+++ src/main/java/hw3/Task3.java
@@ -9,11 +9,11 @@ public class Task3 {
     private Task3() {
     }
 
-    public static HashMap<String, Integer> freqDict(List<?> list) {
-        HashMap< String, Integer> map = new HashMap<>();
-        String s;
+    public static HashMap<Object,Integer> freqDict(List<?> list) {
+        HashMap< Object, Integer> map = new HashMap<>();
+        Object s;
         for (int i = 0; i < list.size(); i++) {
-            s = "\"" + list.get(i).toString() + "\"";
+            s =  list.get(i) ;
             if (map.containsKey(s)) {
                 map.replace(s, map.get(s), map.get(s) + 1);
             } else {
@@ -27,7 +27,7 @@ public class Task3 {
     public static void main(String[] args) {
         List<Integer> list1 = Arrays.asList(1, 2, 1, 1, 1, 2);
         List<String> list2 = Arrays.asList("aa", "aa", "bb", "cc");
-        HashMap< String, Integer> map = freqDict(list2);
+        HashMap< Object, Integer> map = freqDict(list2);
 
         LOGGER.info(map);
     }
