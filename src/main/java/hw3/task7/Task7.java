d290ba4536b6b9c891bdcd2f804c6216ad2faa47
1lostevil1 <ki-04@yandex.ru>
2023-10-28 21:37:21 +0300

    hw3-task7

---
+++ src/main/java/hw3/task7/Task7.java
@@ -0,0 +1,17 @@
+package hw3.task7;
+
+import java.util.TreeMap;
+
+public class Task7 {
+    private Task7() {
+    }
+
+
+    public static void main(String[] args) {
+        TreeMap<String, String> tree = new TreeMap<>(new MyComparator());
+        tree.put("a", "a");
+        tree.put("b", "b");
+        tree.put(null, "n");
+        tree.put("c", "c");
+    }
+}
