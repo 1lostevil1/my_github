ba4c998435bb94c9e1a55ea92c81da4835ed2503
1lostevil1 <ki-04@yandex.ru>
2023-10-28 21:45:21 +0300

    hw3-task7 test

---
+++ src/test/java/hw3/task7/Task7Test.java
@@ -0,0 +1,31 @@
+package hw3.task7;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.TreeMap;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Task7Test {
+
+    @Test
+    @DisplayName(" null test")
+    void NullTest() {
+        TreeMap<String, String> tree = new TreeMap<>(new MyComparator());
+        tree.put("a", "a");
+        tree.put("b", "b");
+        tree.put(null, "n");
+        tree.put("c", "c");
+        assertThat(tree.containsKey(null)).isTrue();
+    }
+
+    @Test
+    @DisplayName(" no null test")
+    void NoNullTest() {
+        TreeMap<String, String> tree = new TreeMap<>(new MyComparator());
+        tree.put("a", "a");
+        tree.put("b", "b");
+        tree.put("d", "d");
+        tree.put("c", "c");
+        assertThat(tree.containsKey(null)).isFalse();
+    }
+}
