9d7f9eed4c7c70ddeefdcb5c1b209f8dde8680db
1lostevil1 <ki-04@yandex.ru>
2023-10-31 22:07:19 +0300

    project2 but not pretty

--- src/main/java/edu/project2/Init.java
+++ src/main/java/edu/project2/Init.java
@@ -1,16 +1,20 @@
 package edu.project2;
 
-import java.util.ArrayList;
+import java.util.Random;
 
 public class Init {
-
     public static void main(String[] args) {
-        int size = 21;
-        Generate a = new Generate(size);
-        a.GEN3000();
-        Generate.Cell start = new Generate.Cell(1,1,false,true,true,false);
-        Generate.Cell finish = new Generate.Cell(7,7,false,false,false,false);
-        a.wayFound(start,finish);
-        a.print();
+        int size = 11;
+            Generate a = new Generate(size);
+            a.GEN3000();
+            Random random = new Random();
+            int rand1 = random.nextInt(1, size - 2);
+            int rand2 = random.nextInt(1, size - 2);
+            if (rand1 % 2 == 0) rand1++;
+            if (rand2 % 2 == 0) rand2++;
+            Generate.Cell start = new Generate.Cell(rand1, rand1, false, true, true, false);
+            Generate.Cell finish = new Generate.Cell(rand2, rand2, false, false, false, false);
+            a.wayFound(start, finish);
+            a.print();
     }
 }
