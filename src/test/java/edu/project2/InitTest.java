900a3d3cbfce039e9256fada02815da2543bd805
1lostevil1 <ki-04@yandex.ru>
2023-11-26 22:48:49 +0300

    project2 - test template

--- src/test/java/edu/project2/InitTest.java
+++ src/test/java/edu/project2/InitTest.java
@@ -6,24 +6,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InitTest {
 
-    @Test
     @RepeatedTest(2000)
     public void someRepeatableTest() {
         Generate a = new Generate(11);
+        //given
         boolean expected = true;
+        //when
         boolean actual = a.gen3000();
+        //then
         assertEquals(expected, actual);
 
     }
 
     @Test
     public void ExceptionTest() {
-        Generate a = new Generate(11);
-        a.gen3000();
+        //given
+        String expectedMessage = "!!CELL IS WALL!!";
         Cell start = new Cell(0, 0, false, true, true, false);
         Cell finish = new Cell(9, 9, false, false, false, false);
+        //when
+        Generate a = new Generate(11);
+        a.gen3000();
+        //then
         Exception exception =  assertThrows(Exception.class, () ->a.wayFound(start,finish));
-        String expectedMessage = "!!CELL IS WALL!!";
         String actualMessage = exception.getMessage();
         assertTrue(actualMessage.contains(expectedMessage));
 
